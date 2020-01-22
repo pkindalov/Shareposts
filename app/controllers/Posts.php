5c4cc8b807df7a0432507e6ba413fb7cc3841730
pkindalov <panayotkindalov@gmail.com>
2020-01-22 21:56:30 +0200

    Updated Posts controller. isLoggedIn has moved from Users controller to session_helper functions

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,5 +1,17 @@
 <?php
     class Posts extends Controller{
+
+        public function __construct()
+        {
+            //Because of this check all methods here are for authenticated users. Else unauthorized user 
+           //will be redirected. 
+           //If you want some actions to not required user authentication then you must MOVE this check from here to 
+           //methods which are requiring authorization
+            if(!isLoggedIn()){
+                redirect('users/login');
+            }
+        }
+
         public function index(){
             $data = [];
            
