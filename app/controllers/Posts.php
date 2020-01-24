33e3ad3844026666f4fed84c7b946aa5496627bd
pkindalov <panayotkindalov@gmail.com>
2020-01-24 19:03:41 +0200

    Added method add in Posts controller.  Added method addPost in Post model.  Added a message when new post is created successfully in main posts page - index.php

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -24,11 +24,58 @@
         }
         
         public function add(){
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
-            $this->view('posts/add', $data);
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //Sanitize post
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                //Validate data
+                if(empty($_POST['title'])){
+                    $data['title_err'] = 'Title cannot be empty';
+                }
+
+                if(empty($_POST['body'])){
+                    $data['body_err'] = 'Body cannot be empty';
+                }
+
+
+                //Make sure no errors
+                if(empty($data['title_err']) && empty($data['body_err']) ){
+                    //Validated
+                    if($this->postModel->addPost($data)){
+                        flash('post_message', 'Post added');
+                        redirect('posts');
+                    } else {
+                        die('Something wrong with adding post');
+                    }
+
+
+                   
+                } else {
+                    //Redirect to same view but with errors
+                    $this->view('posts/add', $data);
+                }
+
+
+
+
+
+            } else {
+
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+                $this->view('posts/add', $data);
+            }
         }
 
 
--- app/models/Post.php
+++ app/models/Post.php
@@ -23,4 +23,22 @@
             return $results;
         }
 
+        public function addPost($post){
+            $this->db->query('INSERT INTO posts (title, body, user_id) 
+                              VALUES (:title, :body, :user_id)
+            ');
+
+            $this->db->bind(':title', $post['title'], null);
+            $this->db->bind(':body', $post['body'], null);
+            $this->db->bind(':user_id', $post['user_id'], null);
+
+            //Execute
+            if($this->db->execute()){
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
     }
