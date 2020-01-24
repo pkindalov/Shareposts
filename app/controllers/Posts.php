bffd4a6941be0ecced2a12cf889c3f281d8830d5
pkindalov <panayotkindalov@gmail.com>
2020-01-24 23:13:06 +0200

    Added edit view,  update metjod in post model and edit method in post model

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -76,6 +76,66 @@
         }
 
 
+        public function edit($postId){
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //Sanitize post
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data = [
+                    'id' => $postId,
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                //Validate data
+                if(empty($_POST['title'])){
+                    $data['title_err'] = 'Title cannot be empty';
+                }
+
+                if(empty($_POST['body'])){
+                    $data['body_err'] = 'Body cannot be empty';
+                }
+
+
+                //Make sure no errors
+                if(empty($data['title_err']) && empty($data['body_err']) ){
+                    //Validated
+                    if($this->postModel->updatePost($data)){
+                        flash('post_message', 'Post updated');
+                        redirect('posts');
+                    } else {
+                        die('Something wrong with adding post');
+                    }
+
+
+                   
+                } else {
+                    //Redirect to same view but with errors
+                    $this->view('posts/edit', $data);
+                }
+
+            } else {
+                //Get existing posts from the model
+                $post = $this->postModel->getPostById($postId);
+
+                //Check for owner
+                if($post->user_id != $_SESSION['user_id']){
+                    redirect('posts');
+                }
+
+                $data = [
+                    'id' => $postId,
+                    'title' => $post->title,
+                    'body' => $post->body
+                ];
+                $this->view('posts/edit', $data);
+            }
+        }
+
         public function show($id){
             $post = $this->postModel->getPostById($id);
             $user = $this->userModel->getUserById($post->user_id);
--- app/models/Post.php
+++ app/models/Post.php
@@ -1,14 +1,16 @@
 <?php
-    class Post{
-        private $db;
+class Post
+{
+    private $db;
 
-        public function __construct()
-        {
-            $this->db = new Database;
-        }
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
 
-        public function getPosts(){
-            $this->db->query("SELECT *, 
+    public function getPosts()
+    {
+        $this->db->query("SELECT *, 
                                 posts.id AS postId, 
                                 users.id AS userId,
                                 posts.created_at AS postCreated,
@@ -18,36 +20,53 @@
                               ON posts.user_id = users.id
                               ORDER BY posts.created_at DESC
                               ");
-            $results = $this->db->resultSet();
+        $results = $this->db->resultSet();
 
-            return $results;
-        }
+        return $results;
+    }
 
-        public function addPost($post){
-            $this->db->query('INSERT INTO posts (title, body, user_id) 
+    public function addPost($post)
+    {
+        $this->db->query('INSERT INTO posts (title, body, user_id) 
                               VALUES (:title, :body, :user_id)
             ');
 
-            $this->db->bind(':title', $post['title'], null);
-            $this->db->bind(':body', $post['body'], null);
-            $this->db->bind(':user_id', $post['user_id'], null);
+        $this->db->bind(':title', $post['title'], null);
+        $this->db->bind(':body', $post['body'], null);
+        $this->db->bind(':user_id', $post['user_id'], null);
 
-            //Execute
-            if($this->db->execute()){
-                return true;
-            } else {
-                return false;
-            }
-
+        //Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
         }
-
-        public function getPostById($id){
-            $this->db->query("SELECT * FROM posts WHERE id = :postId");
-
-            $this->db->bind(':postId', $id, null);
-            $row = $this->db->single();
-
-            return $row;
-        }
-
     }
+
+    public function updatePost($post)
+    {
+        $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :postId
+            ');
+
+        $this->db->bind(':postId', $post['id'], null);
+        $this->db->bind(':title', $post['title'], null);
+        $this->db->bind(':body', $post['body'], null);
+
+        //Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getPostById($id)
+    {
+        $this->db->query("SELECT * FROM posts WHERE id = :postId");
+
+        $this->db->bind(':postId', $id, null);
+        $row = $this->db->single();
+
+        return $row;
+    }
+}
