2113a9a7df2cd442f6275657723646a968cde82d
pkindalov <panayotkindalov@gmail.com>
2020-01-28 21:42:32 +0200

    Updated user model.  Added new method getUsersPaginated to return list of users but paginated. Other added methods are - getUserPostsCount and  getUsersPostWithPag

--- app/models/User.php
+++ app/models/User.php
@@ -1,64 +1,114 @@
 <?php
-    class User{
-        private $db;
+class User
+{
+    private $db;
 
-        public function __construct()
-        {
-            $this->db = new Database;
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    //Registering User
+    public function register($data)
+    {
+        $this->db->query("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+
+        //Bind values
+        $this->db->bind(":name", $data['name'], null);
+        $this->db->bind(":email", $data['email'], null);
+        $this->db->bind(":password", $data['password'], null);
+
+        //Execute query
+        if ($this->db->execute()) {
+            return true;
         }
 
-        //Registering User
-        public function register($data){
-            $this->db->query("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
-
-            //Bind values
-            $this->db->bind(":name", $data['name'], null);
-            $this->db->bind(":email", $data['email'], null); 
-            $this->db->bind(":password", $data['password'], null);
-
-            //Execute query
-            if($this->db->execute()){
-                return true;
-            }
-
-            return false;
-        }
+        return false;
+    }
 
 
-        //Login User
-        public function login($email, $password){
-            $this->db->query("SELECT * FROM users WHERE email = :email");
-            $this->db->bind(':email', $email, null);
+    //Login User
+    public function login($email, $password)
+    {
+        $this->db->query("SELECT * FROM users WHERE email = :email");
+        $this->db->bind(':email', $email, null);
 
-            $row = $this->db->single();
-            $hashed_password = $row->password;
+        $row = $this->db->single();
+        $hashed_password = $row->password;
 
-            if(password_verify($password, $hashed_password)){
-                return $row;
-            } else {
-                return false;
-            }
-        }
-
-        //Find user by email
-        public function findUserByEmail($email){
-            $this->db->query("SELECT * FROM users WHERE email = :email");
-            $this->db->bind(':email', $email, null);
-
-            $row = $this->db->single();
-
-            //Check row
-            if($this->db->rowCount() > 0){
-                return true;
-            }
-
-            return false;
-        }
-
-        public function getUserById($userId){
-            $this->db->query("SELECT * FROM users WHERE id = :userId");
-            $this->db->bind(':userId', $userId, null);
-            $row = $this->db->single();
+        if (password_verify($password, $hashed_password)) {
             return $row;
+        } else {
+            return false;
         }
     }
+
+    //Find user by email
+    public function findUserByEmail($email)
+    {
+        $this->db->query("SELECT * FROM users WHERE email = :email");
+        $this->db->bind(':email', $email, null);
+
+        $row = $this->db->single();
+
+        //Check row
+        if ($this->db->rowCount() > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getUserById($userId)
+    {
+        $this->db->query("SELECT * FROM users WHERE id = :userId");
+        $this->db->bind(':userId', $userId, null);
+        $row = $this->db->single();
+        return $row;
+    }
+
+    public function getUsersPaginated($page, $pageSize)
+    {
+        if ($page <= 0) {
+            $page = 1;
+        }
+
+        // $allRecords = $this->getPosts();
+        $offset = ($page - 1) * $pageSize;
+
+        // echo $offset;
+        $this->db->query("SELECT *
+                                FROM users 
+                                WHERE users.role = 'user'
+                                LIMIT :limit
+                                OFFSET :offset
+          ");
+
+        $this->db->bind(':limit', $pageSize, null);
+        $this->db->bind(':offset', $offset, null);
+        $users = $this->db->resultSet();
+        return $users;
+    }
+
+    public function getUserPostsCount($userId){
+        $this->db->query("SELECT * FROM posts WHERE posts.user_id = :userId");
+        $this->db->bind(":userId", $userId, null);
+        $posts = $this->db->resultSet();
+        $postsCount = $this->db->rowCount(); 
+        return $postsCount;
+
+    }
+
+    public function getUsersPostWithPag($userId, $page, $pageSize){
+        $page  = $page == 0 ? 1 : $page;
+        $offset = ($page - 1) * $pageSize;
+        $this->db->query("
+            SELECT posts.*, users.name FROM posts INNER JOIN users ON users.id = posts.user_id WHERE posts.user_id = :userId LIMIT :limit OFFSET :offset
+        ");
+        $this->db->bind(":userId", $userId, null);
+        $this->db->bind(":limit", $pageSize, null);
+        $this->db->bind(":offset", $offset, null);
+        $result = $this->db->resultSet();
+        return $result;
+    }
+}
