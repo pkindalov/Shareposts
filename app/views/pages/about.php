7444700ddb5ba31322ad51ed71b4571d57ed62cb
pkindalov <panayotkindalov@gmail.com>
2020-01-30 22:18:27 +0200

    Updates in main posts file. Not permit to like or dislike your own posts

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,4 +1,11 @@
 <?php require_once APPROOT . '/views/inc/header.php' ?>
 <h1><?php echo $data['title']; ?></h1>
 <p><?php echo $data['description']; ?></p>
+<p>
+# Shareposts
+This is a simple CRUD application based on the following course to Udemy - https://www.udemy.com/course/object-oriented-php-mvc/  This version is updated with pagination for the posts, added roles for the users - for the moment they are 2 types (users and admin). Admin can see all users and to edit or delete bad posts. Adde also likes for the posts - like and dislike buttons
+
+THE PROJECT IS STILL IN PROCESS OF DEVELOPMENT YET.
+
+</p>
 <?php require_once APPROOT . '/views/inc/footer.php' ?>
--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -14,39 +14,59 @@
     </div>
 </div>
 <?php foreach ($data['posts'] as $post) : ?>
-    <div class="card card-body mb-3">
+    <?php if($post->userId == $_SESSION['user_id']) : ?>
+        <div class="card card-body mb-3 bg-info">
+            
+            <?php else : ?>
+                <div class="card card-body mb-3">
+    
+    <?php endif; ?>
+    
         <h4 class="card-title">
             <?php echo $post->title; ?>
         </h4>
         <div class="bg-light p-2 mb-3">
-            <p>This post has <strong><?php echo $post->totalVotes; ?></strong> </p>
+            <p>This post has <strong><?php echo $post->totalVotes; ?></strong> likes </p>
             Written By <?php echo $post->name ?> on <?php echo $post->postCreated; ?>
         </div>
         <p class="card-text">
             <?php echo $post->body ?>
         </p>
-        <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark"><i class="fa">&#xf05a;</i>More...</a>
+        <div>
+            <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark pull-left mb-2"><i class="fa">&#xf05a;</i>More...</a>
 
-        <?php if ($post->voted && (int) $post->voted === (int) $_SESSION['user_id']) : ?>
-            <a href="<?php echo URLROOT; ?>/likes/dislikePost/<?php echo $post->postId; ?>" class="btn btn-primary"><i class="fas">&#xf165;</i>Dislike</a>
-        <?php else : ?>
-            <a href="<?php echo URLROOT; ?>/likes/addLikeToPost/<?php echo $post->postId; ?>" class="btn btn-primary"><i class="fa fa-heart"></i>Like</a>
-        <?php endif; ?>
+
+            <?php if ($post->voted && $post->voted ===  $_SESSION['user_id'] && $post->userId != $_SESSION['user_id']) : ?>
+               
+                <a href="<?php echo URLROOT; ?>/likes/dislikePost/<?php echo $post->postId; ?>" class="btn btn-primary pull-right"><i class="fas">&#xf165;</i>Dislike</a>
+
+            <?php else : ?>
+
+                <?php if($post->userId != $_SESSION['user_id']) : ?>
+                <a href="<?php echo URLROOT; ?>/likes/addLikeToPost/<?php echo $post->postId; ?>" class="btn btn-primary pull-right"><i class="fa fa-heart"></i>Like</a>
+                <?php endif; ?>
+        </div>
+    <?php endif; ?>
     </div>
 <?php endforeach; ?>
 
-<?php if ($data['hasPrevPage']) : ?>
-    <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['prevPage']; ?>" class="btn btn-primary pull-left">
-        <i class="fa fa-backward"></i> Prev
-    </a>
-<?php endif; ?>
+<div class="row">
+    <div class="col-md-12">
 
-<?php if ($data['hasNextPage']) : ?>
-    <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['nextPage']; ?>" class="btn btn-primary pull-right">
-        <i class="fa fa-forward"></i> Next
-    </a>
-<?php endif; ?>
 
+        <?php if ($data['hasPrevPage']) : ?>
+            <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['prevPage']; ?>" class="btn btn-primary pull-left">
+                <i class="fa fa-backward"></i> Prev
+            </a>
+        <?php endif; ?>
+
+        <?php if ($data['hasNextPage']) : ?>
+            <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['nextPage']; ?>" class="btn btn-primary pull-right">
+                <i class="fa fa-forward"></i> Next
+            </a>
+        <?php endif; ?>
+    </div>
+</div>
 
 
 <?php require_once APPROOT . '/views/inc/footer.php' ?>
