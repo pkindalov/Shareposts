9f5c97f324f9e165bd76e1ae46d9e1b9e88ad6ae
pkindalov <panayotkindalov@gmail.com>
2020-01-30 22:43:23 +0200

    Added missing closing div tag and formatted file

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -14,59 +14,63 @@
     </div>
 </div>
 <?php foreach ($data['posts'] as $post) : ?>
-    <?php if($post->userId == $_SESSION['user_id']) : ?>
+    <?php if ($post->userId == $_SESSION['user_id']) : ?>
         <div class="card card-body mb-3 bg-info">
+
+        <?php else : ?>
+            <div class="card card-body mb-3">
+
+            <?php endif; ?>
+
+            <h4 class="card-title">
+                <?php echo $post->title; ?>
+            </h4>
+            <div class="bg-light p-2 mb-3">
+                <p>This post has <strong><?php echo $post->totalVotes; ?></strong> likes </p>
+                Written By <?php echo $post->name ?> on <?php echo $post->postCreated; ?>
+            </div>
+            <p class="card-text">
+                <?php echo $post->body ?>
+            </p>
+            <div>
+                <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark pull-left mb-2"><i class="fa pr-2">&#xf05a;</i>More...</a>
             
-            <?php else : ?>
-                <div class="card card-body mb-3">
-    
-    <?php endif; ?>
-    
-        <h4 class="card-title">
-            <?php echo $post->title; ?>
-        </h4>
-        <div class="bg-light p-2 mb-3">
-            <p>This post has <strong><?php echo $post->totalVotes; ?></strong> likes </p>
-            Written By <?php echo $post->name ?> on <?php echo $post->postCreated; ?>
-        </div>
-        <p class="card-text">
-            <?php echo $post->body ?>
-        </p>
-        <div>
-            <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark pull-left mb-2"><i class="fa">&#xf05a;</i>More...</a>
 
 
-            <?php if ($post->voted && $post->voted ===  $_SESSION['user_id'] && $post->userId != $_SESSION['user_id']) : ?>
-               
-                <a href="<?php echo URLROOT; ?>/likes/dislikePost/<?php echo $post->postId; ?>" class="btn btn-primary pull-right"><i class="fas">&#xf165;</i>Dislike</a>
+                <?php if ($post->voted && $post->voted ===  $_SESSION['user_id'] && $post->userId != $_SESSION['user_id']) : ?>
 
-            <?php else : ?>
+                    <a href="<?php echo URLROOT; ?>/likes/dislikePost/<?php echo $post->postId; ?>" class="btn btn-primary pull-right"><i class="fas pr-2">&#xf165;</i>Dislike</a>
+                    </div>
 
-                <?php if($post->userId != $_SESSION['user_id']) : ?>
-                <a href="<?php echo URLROOT; ?>/likes/addLikeToPost/<?php echo $post->postId; ?>" class="btn btn-primary pull-right"><i class="fa fa-heart"></i>Like</a>
+                <?php else : ?>
+
+                    <?php if ($post->userId != $_SESSION['user_id']) : ?>
+                        <a href="<?php echo URLROOT; ?>/likes/addLikeToPost/<?php echo $post->postId; ?>" class="btn btn-primary pull-right"><i class="fa fa-heart pr-2"></i>Like</a>
+                    <?php endif; ?>
+            </div>
+           
+        <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+
+
+        <div class="row">
+            <div class="col-md-12">
+
+
+                <?php if ($data['hasPrevPage']) : ?>
+                    <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['prevPage']; ?>" class="btn btn-primary pull-left">
+                        <i class="fa fa-backward"></i> Prev
+                    </a>
                 <?php endif; ?>
+
+                <?php if ($data['hasNextPage']) : ?>
+                    <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['nextPage']; ?>" class="btn btn-primary pull-right">
+                        <i class="fa fa-forward"></i> Next
+                    </a>
+                <?php endif; ?>
+            </div>
         </div>
-    <?php endif; ?>
-    </div>
-<?php endforeach; ?>
-
-<div class="row">
-    <div class="col-md-12">
 
 
-        <?php if ($data['hasPrevPage']) : ?>
-            <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['prevPage']; ?>" class="btn btn-primary pull-left">
-                <i class="fa fa-backward"></i> Prev
-            </a>
-        <?php endif; ?>
-
-        <?php if ($data['hasNextPage']) : ?>
-            <a href="<?php echo URLROOT; ?>/posts/getPage/<?php echo $data['nextPage']; ?>" class="btn btn-primary pull-right">
-                <i class="fa fa-forward"></i> Next
-            </a>
-        <?php endif; ?>
-    </div>
-</div>
-
-
-<?php require_once APPROOT . '/views/inc/footer.php' ?>
+        <?php require_once APPROOT . '/views/inc/footer.php' ?>
